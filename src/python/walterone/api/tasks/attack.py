443d6de2523d6a64ab7bf2e2b86fde1cfde8b182
ewokcillo <azotillo@gmail.com>
2022-08-25 16:35:39 +0200

    Fixed missing defend relation in attack task

--- src/python/walterone/api/tasks/attack.py
+++ src/python/walterone/api/tasks/attack.py
@@ -1,10 +1,11 @@
+from api.models.action import Defend
 from api.models.match import MatchIA
 from walterone.celery import app
 
 @app.task(bind=True)
 def attack_task(self, attacked_ia_id, match_id, damage):
     match_ia = MatchIA.objects.get(ia=attacked_ia_id, match=match_id)
-    reduced = match_ia.defend.shield if match_ia.defend and match_ia.defend.active else 0
+    reduced = match_ia.defend.shield if Defend.objects.filter(match_ia=match_ia) and match_ia.defend.active else 0
     match_ia.life -= damage + reduced
     if match_ia.life <= 0:
         match_ia.alive = False

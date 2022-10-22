ebb0a1a4fc04364db613bf5f092ee239cfd6acf7
Sergio Soto Núñez <sergio.soto@sngular.com>
2022-10-22 17:49:07 +0200

    Deleted imports

--- src/python/walterone/api/models/action.py
+++ src/python/walterone/api/models/action.py
@@ -1,8 +1,4 @@
-import logging
-from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
 from api.models.match import MatchIA
 

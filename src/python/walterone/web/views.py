4b39fc28e1071b8952c75df116da8de1942d70e1
ewokcillo <diego.pascual@sngular.com>
2023-10-20 11:52:22 +0200

    refactor info match.remove match_zones

--- src/python/walterone/web/views.py
+++ src/python/walterone/web/views.py
@@ -13,34 +13,20 @@ ES_TIMEZONE = pytz.timezone('Europe/Madrid')
 
 def info_match(request, match_id):
     match = get_object_or_404(Match, id=match_id)
-    template = loader.get_template("match_zones.html")
+    template = loader.get_template("info_match.html")
     context = {
         "match_name": match.name,
         "refresh": request.GET.get('refresh', 2),
         "datetime": datetime.now(ES_TIMEZONE).isoformat()
     }
 
-    ias = []
-    for match_ia in match.matchia_set.order_by("-life"):
-        ias.append({
-            "name": match_ia.ia.name,
-            "id": match_ia.id,
-            "zone": match_ia.where_am_i.name,
-            "life": match_ia.life,
-            "lucky_unlucky": match_ia.lucky_unlucky,
-            "go_ryu": match_ia.go_ryu,
-        })
-
-    context["match_ias"] = ias
+    context["match_ias"] = [
+        match_ia for match_ia in match.matchia_set.order_by("-life")]
 
     attacks_limit = request.GET.get("attacks_limit", 15)
-    attacks_qs = match.attack_set.order_by("-timestamp")[:attacks_limit]
+    context["attacks"] = [
+        attack for attack in match.attack_set.order_by("-timestamp")[:attacks_limit]]
 
-    attacks = []
-    for attack in attacks_qs:
-        attacks.append(attack)
-
-    context["attacks"] = attacks
     return HttpResponse(template.render(context, request))
 
 

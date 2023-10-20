c5d83ceac0279ce297b1038f4ad610980da49810
ewokcillo <diego.pascual@sngular.com>
2023-10-20 10:18:36 +0200

    Add template for info_match

--- src/python/walterone/web/urls.py
+++ src/python/walterone/web/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('<int:match_id>/zones/', views.zones, name='zones_match'),
+    path('<int:match_id>/info_match/', views.info_match, name='info_match'),
     path('info/', views.info, name='info'),
 ]

c3ad2dddeb69499e72761e1ff75bec1d7a3585d6
Sergio Soto <sergio.soto.externos@grupobme.es>
2023-10-21 19:41:52 +0200

    Enables graph chart view

--- src/python/walterone/web/urls.py
+++ src/python/walterone/web/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('<int:match_id>/info_match/', views.info_match, name='info_match'),
     path('info/', views.info, name='info'),
+    path('graph/<int:match_id>', views.graph, name='graph'),
+    path('graph_data/<int:match_id>', views.match_data, name='graph-data'),
 ]
--- src/python/walterone/web/views.py
+++ src/python/walterone/web/views.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import pytz
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404
 
 
 from api.models.match import Match
+from api.models.zone import Zone
 from api.models.action import FinalDamage
 
 ES_TIMEZONE = pytz.timezone('Europe/Madrid')
@@ -35,3 +36,54 @@ def info(request):
     template = loader.get_template("info.html")
 
     return HttpResponse(template.render({}, request))
+
+
+def graph(request, match_id):
+    template = loader.get_template("graph.html")
+    match = get_object_or_404(Match, id=match_id)
+
+    context = {
+        "match": match
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def match_data(request, match_id):
+    match = get_object_or_404(Match, id=match_id)
+
+    nodes = []
+    links = []
+    for zone in Zone.objects.filter(map=match.map):
+        nodes.append(
+            {"name": zone.name.capitalize(), "group": 1}
+        )
+        for n in zone.neighbors.all():
+            links.append(
+                {"source": zone.name.capitalize(),
+                 "target": n.name.capitalize(),
+                 "value": 1}
+            )
+
+    # nodes = [
+    #     {"name": "Myriel", "group": 1},
+    #     {"name": "Napoleon", "group": 1},
+    #     {"name": "Mlle.Baptistine", "group": 1},
+    #     {"name": "Mme.Magloire", "group": 1},
+    #     {"name": "CountessdeLo", "group": 1},
+    # ]
+    # links = [
+    #     {"source": 1, "target": 0, "value": 1},
+    #     {"source": 2, "target": 0, "value": 8},
+    #     {"source": 3, "target": 0, "value": 10},
+    #     {"source": 3, "target": 2, "value": 6},
+    #     {"source": 4, "target": 0, "value": 1},
+    #     {"source": 5, "target": 0, "value": 1},
+    # ]
+    return JsonResponse(
+        {
+            "nodes": nodes,
+            "links": links,
+        },
+        content_type="application/json"
+    )
